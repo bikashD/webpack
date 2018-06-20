de362d7f6b9ed5891925e7485278a08c047f4dae
bikashD <bikash@meawwworld.com>
2018-06-20 15:10:50 +0530

    fixed the output file name issue

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const path = require('path');
 
 module.exports = {
 	entry: './src/index.js',
-	output: path.resolve(__dirname, 'dist'),
-	filename: 'bundle.js'
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js'
+	}
 };
